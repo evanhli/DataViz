39b2bb3114c4c1621fa1bc30d8415ecd40d47e5e
evanhli <evan.li.2014@gmail.com>
2019-09-15 11:26:03 -0400

    Refactor

--- src/Components/LineGraph.js
+++ src/Components/LineGraph.js
@@ -6,7 +6,7 @@ function LineGraph() {
   useEffect(() => {
     d3.csv('http://localhost:4000/static/data/test.csv')
       .then(data => {
-        console.log(data);
+        // console.log(data);
         const close = data.map(data => data.close);
         const x = d3.scaleLinear().domain([24000, d3.max(close)]).range([0, 800])
         d3.select('.chart')
@@ -14,23 +14,9 @@ function LineGraph() {
           .data(close.slice(0, 10))
             .enter()
             .append('div')
-            .style('width', (d) => {
-              return x(d) + 'px'
-            })
+            .style('width', (d) => x(d) + 'px')
             .text(d => d)
       });
-  // if (graphContainer.current) {
-    //   const data = [12, 5, 6, 6, 9, 10];
-    //   // Select or create dom node, append an SVG to it with some attributes
-    //   d3.select(graphContainer.current)
-    //     .selectAll('div')
-    //     .data(data)
-    //       .enter()
-    //       .append('div')
-    //       .style('width', (d) => d * 10 + 'px')
-    //       .text(d => d);
-    // }
-
   })
   return <div className='chart' ref={graphContainer}></div>
 }
